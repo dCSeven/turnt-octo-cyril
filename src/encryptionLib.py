840c6ba87ad7c077f456a8c6cfbb0e1cedd57c70
dCSeven <dCSeven@users.noreply.github.com>
2014-01-09 13:07:02 +0100

    Update encryptionLib.py
    Added subtractStrings method

--- src/encryptionLib.py
+++ src/encryptionLib.py
@@ -174,12 +174,18 @@ def rand_enc(data,key):
 	else:
 		print "Key must be int"
 		return "invalid"
+<<<<<<< HEAD
 	random.shuffle(data) # change to something more reproduceable
 	return data
 	
 def rand_dec(data,key):
 	return 'not_done_yet'
 =======
+=======
+
+#### Tools
+
+>>>>>>> Update encryptionLib.py
 # returns a table of 256 fields for each possible value
 def histogramm(data):
 	if isinstance(data,str):
@@ -189,5 +195,23 @@ def histogramm(data):
 		return charTable
 	else:
 		print "data must be string"
+		return "invalid"
+<<<<<<< HEAD
+>>>>>>> Update encryptionLib.py
+=======
+
+# subtracts the srings (only the letters)
+# if the strings don't have the same size the shorter will be padded with A (means Zero)
+def subtractStrings(minuend,subtrahend):
+	if isinstance(minuend,str) and isinstance(subtrahend,str):
+		length=len(minuend)-len(subtrahend)
+		if(length<0): #subtrahend longer
+			minuend+=''.zfill(-length)
+		elif(length>0):#minuend longer
+			subtrahend+=''.zfill(-length)
+		return map(lambda x,y:ord(x)-ord(y) if isinstance(x,str) and isinstance(y,str) else 0,minuend,subtrahend)
+		# TODO  add check if it is a letter and maybe add 'A' and call chr
+	else:
+		print "both parameters must be strings"
 		return "invalid"
 >>>>>>> Update encryptionLib.py
