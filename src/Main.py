dfe1b5eb580f1b4915e43d1917c6f618f97e72e2
dCSeven <dopoflor@gmail.com>
2014-02-10 11:49:41 +0100

    Added parseParams Method to parse commandline arguments also extended .gitignore

--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,12 @@
 #!py -2
+# FIXME -d requires argument, but shouldn't (ArgumentParser)
+# TODO add more choices to the ArgumentParser encryptionMethod
+# TODO parse arguments in readParams
 
 from array import array
+
+import argparse
+import sys
 
 from encryptionLib import *	# encryption stuff is in encryptionLib.py
 
@@ -10,12 +16,29 @@ decryption=vigenere_dec
 data="this is sparta98"
 
 #key=5
-key="cxfk go95"
+key="cxfk go9"
 
+def readParams():
+	parser=argparse.ArgumentParser(description='A simple En-/Decryption library')
+	parser.add_argument(
+		'--plainFile', '-p', nargs='?', default=sys.stdin, type=argparse.FileType('r'),
+		help='the plaintextfile to be encoded')
+	parser.add_argument(
+		'--out', nargs='?', default=sys.stdout, type=argparse.FileType('w'),
+		help='the file where the encoded stuff should be written')
+	parser.add_argument(
+		'encryptionMethod', nargs='?', 
+		choices=['caesar_enc', 'caesar_dec', 'vigenere_enc', 'vigenere_dec', 'vernam', 'vernam_h_enc', 'vernam_h_dec'])
+	parser.add_argument(
+		'-d','--debug', help='Debug') # FIXME requires argument, but shouldn't
+	args = parser.parse_args()
+#	args.log.write('%s' % sum(args.integers))
+#	args.log.close()
 
 
 def main():
 	global encryption,data,key
+	readParams()
 	#print 'Current data is\n'+data+' with key: '+str(key)
 	print 'Key: '+str(key)
 	print 'Text:'+data
